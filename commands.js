2f13365902155e38e55757a34a34b87029a3c737
SafetyFlux <dekuwither@gmail.com>
2023-08-01 11:10:23 -0600

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,15 @@
+// Package imports
+import 'dotenv/config';
+// App imports
+import { InstallGlobalCommands } from './utils';
+
+// Simple test command
+const TEST_COMMAND = {
+  name: 'test',
+  description: 'Basic command',
+  type: 1,
+};
+
+const ALL_COMMANDS = [TEST_COMMAND];
+
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
